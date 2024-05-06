941b7b8cb1d1bc7b494eb90ddf06faa6ee6a913d
Luiz Filipe <luizfilipemr17@gmail.com>
2024-05-06 14:32:57 -0300

    Allocating threads dynamically with Thread Pool

--- src/br/com/alura/server/Main.java
+++ src/br/com/alura/server/Main.java
@@ -4,6 +4,8 @@ package br.com.alura.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -11,14 +13,15 @@ public class Main {
         System.out.println("--- Starting server ---");
         ServerSocket server = new ServerSocket(9090);
 
+        // ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
         while (true){
             Socket socket = server.accept();
             System.out.println("Accepting new connection from " + socket.getPort());
 
             DistributeTasks distributedServer = new DistributeTasks(socket);
-            Thread clientThread = new Thread(distributedServer);
-            clientThread.start();
-
+            threadPool.execute(distributedServer);
         }
     }
 }
