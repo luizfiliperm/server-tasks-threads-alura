dc284c78e0511791d63abb7dace99787acbe9839
Luiz Filipe <luizfilipemr17@gmail.com>
2024-05-06 15:49:08 -0300

    Returning info

--- src/br/com/alura/server/DistributeTasks.java
+++ src/br/com/alura/server/DistributeTasks.java
@@ -1,6 +1,7 @@
 package br.com.alura.server;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -18,14 +19,29 @@ public class DistributeTasks implements Runnable {
         System.out.println("Distributing tasks to " + socket);
 
         Scanner sc = new Scanner(socket.getInputStream());
+        PrintStream ps = new PrintStream(socket.getOutputStream());
 
-        while (sc.hasNextLine()) {
-            String command = sc.nextLine();
-            System.out.println("Received command: " + command);
-        }
+            while (sc.hasNextLine()) {
+                String command = sc.nextLine();
+                System.out.println("Command received: " + command);
+                switch (command) {
+                    case "c1":
+                        ps.println("Command 1 received");
+                        break;
+                    case "c2":
+                        ps.println("Command 2 received");
+                        break;
+                    case "exit":
+                        ps.println("Exiting...");
+                        break;
+                    default:
+                        ps.println("Command not found");
+                        break;
+                }
+            }
 
-
-        sc.close();
+            ps.close();
+            sc.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
